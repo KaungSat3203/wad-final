41a2e05b327e49c75126d8b56ca26ecc4602a123
KaungSat3203 <kaungsat3203@gmail.com>
2025-10-02 11:00:45 +0700

    Refactor next.config.mjs for improved readability and formatting

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
-
-  basePath: '/fin-customer', 
-
+  basePath: '/fin-customer',
   experimental: { 
-
     instrumentationHook: true, 
-
-  }, 
-
-}; 
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/fin-customer/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
